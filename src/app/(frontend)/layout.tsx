329d74e830147e564a22f5c9a28d97d8d22d3b71
xugeboy <xugeboy@gmail.com>
2026-03-13 11:10:50 +0800

    modified:   .gitignore 	new file:   next-env.d.ts 	new file:   package-lock.json 	new file:   public/apple-touch-icon.png 	new file:   public/favicon-96x96.png 	modified:   public/favicon.ico 	modified:   public/favicon.svg 	new file:   public/pioneers-logo-white.png 	new file:   public/pioneers-logo.png 	new file:   public/site.webmanifest 	new file:   public/web-app-manifest-192x192.png 	new file:   public/web-app-manifest-512x512.png 	deleted:    public/website-template-OG.webp 	modified:   src/app/(frontend)/layout.tsx 	modified:   src/app/(payload)/admin/importMap.js 	modified:   src/payload-types.ts

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -23,8 +23,12 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
       <head>
         <InitTheme />
-        <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <Providers>
